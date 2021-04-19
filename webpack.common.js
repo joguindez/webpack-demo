5f42c3a8c692b32f8cefe17636a4dc9b3d2eff53
Jonh <hernandez.guille.jon@gmail.com>
2021-04-18 22:13:00 -0400

    Broke the webpack.config file and added three files webpack.prod webpack.dev webpack.common files.

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,8 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: false,
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
